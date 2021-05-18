e7b2e9aaf1ef1ae9704cef7bbdb5e5e7abe2083c
21livredhi <alsp44hyf7488@126.com>
2021-05-18 10:02:06 +0800

    green1.0

--- App/Common/Conf/db.php
+++ App/Common/Conf/db.php
@@ -3,7 +3,7 @@
 return array(
     'DB_TYPE' => 'mysqli', // 数据库类型
     'DB_HOST' => '120.24.226.7', // 服务器地址
-    'DB_NAME' => 'auth', // 数据库名
+    'DB_NAME' => 'green', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'fuck1221924', // 密码
     'DB_PORT' => '3306', // 端口
